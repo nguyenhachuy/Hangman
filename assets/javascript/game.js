937e51e0977c56fac8dcb67c52ebffad8969895b
Gus Nguyen <nguyenhachuy@gmail.com>
2017-09-04 22:38:53 -0700

    Kinda working game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,9 +1,22 @@
-function userGuess() {
-	console.log(this.innerHTML);
-}
+var word = "lmao";
 
 var buttons = document.getElementsByTagName("button");
 
 for(var i = 0; i < buttons.length; i++) {
 	buttons[i].addEventListener("click", userGuess);
+}
+
+document.getElementById("guessRow").innerHTML = prepWord(this);
+
+function userGuess() {
+	console.log(this.innerHTML);
+}
+
+function prepWord() {
+	console.log("prepWord");
+	var result = "";
+	for(var i = 0; i < word.length; i++) {
+		result += "_ ";
+	}
+	return result;
 }
