552a11ff99cd3a12225277910db50b0eeca72893
nguyenhachuy <nguyenhachuy@gmail.com>
2017-09-06 01:07:47 -0700

    Adding event handlers for buttons

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,15 +1,22 @@
 var word = "lmao";
-
+var wordArray = ["L","M","A","O"];
 var buttons = document.getElementsByTagName("button");
 
 for(var i = 0; i < buttons.length; i++) {
-	buttons[i].addEventListener("click", userGuess);
+	buttons[i].addEventListener("click", checkUserGuess);
 }
 
 document.getElementById("guessRow").innerHTML = prepWord(this);
 
-function userGuess() {
-	console.log(this.innerHTML);
+function checkUserGuess() {
+	var guess = this.innerHTML;
+	console.log(guess);
+	var result = false;
+	if(wordArray.includes(guess)) {
+		result = true;
+	}
+	updateButtonUI(guess, result);
+
 }
 
 function prepWord() {
@@ -19,4 +26,18 @@ function prepWord() {
 		result += "_ ";
 	}
 	return result;
+}
+
+function main() {
+	checkUserGuess();
+}
+
+function updateButtonUI(button, status) {
+	var update;
+	if(status) {
+		var update = ` disabled`;
+	}
+	else update = ` btn-lg`;
+	document.getElementById(`char${button}`).className += update;
+	console.log(document.getElementById(`char${button}`).innerHTML);
 }
