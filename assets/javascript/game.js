7c9f94210fdc635925ed83aef41cbbeed2d98f72
Gus Nguyen <nguyenhachuy@gmail.com>
2017-09-08 18:40:15 -0700

    Convereted to OOP

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,86 +1,88 @@
-var word = "Poopybutthole".toUpperCase();
-var wordArray;
-var guessArray;
-var buttons = document.getElementsByTagName("button");
-var state = 0;
-
+var buttons = document.getElementsByClassName("input-button");
+var dict = [
+	"Uncle Steve",
+	"Cousin Nicky",
+	"Beauregard",
+	"Frankenstein",
+	"Sleepy Gary",
+	"Photography Raptor",
+	"Pencilvestyr",
+	"Tinkles",
+	"Hamurai",
+	"Amish Cyborg",
+	"Reverse Giraffe",
+	"Ghost in a Jar",
+	"Baby Wizard",
+	"Duck With Muscles"
+];
 /* Setting up stuffs */
 
-for(var i = 0; i < buttons.length; i++) {
-	buttons[i].addEventListener("click", checkUserGuess);
-}
+// document.getElementById("play-button").addEventListener("click", prepWord);
 
-document.getElementById("play-button").addEventListener("click", prepWord);
-
-document.getElementById("reset-button").addEventListener("click", resetGame);
 
  window.onload = function() {
 	$('#welcome-modal').modal(show = true);
  }
 /* Code body */
 
-function checkUserGuess() {
-	var guess = this.innerHTML;
-	//console.log(guess);
-	var result = false;
-	if(wordArray.includes(guess)) {
-		result = true;
-		updateGuessUI(guess);
-		document.getElementById("guessRow").innerHTML = updateGuessWord();
-	}
-	else
-		updateHangmanUI();
+// function checkUserGuess() {
+// 	var guess = this.innerHTML;
+// 	console.log(guess);
+// 	var result = false;
+// 	if(wordArray.includes(guess)) {
+// 		result = true;
+// 		updateGuessUI(guess);
+// 		document.getElementById("guessRow").innerHTML = updateGuessWord();
+// 	}
+// 	else
+// 		updateHangmanUI();
 
-	updateButtonUI(guess, result);
-	updateGameStatus();
-}
+// 	updateButtonUI(guess, result);
+// 	updateGameStatus();
+// }
 
 
-function prepWord() {
+// function prepWord() {
 
-	//console.log("prepWord");
-	wordArray = word.split("");
-	guessArray = new Array(wordArray.length);
-	guessArray.fill(false);
-	for(var i = 0; i < guessArray.length; i++) {
-		if(wordArray[i] === "-") {
-			guessArray[i] = true;
-		}
-	}
+// 	//console.log("prepWord");
+// 	wordArray = word.split("");
+// 	guessArray = new Array(wordArray.length);
+// 	guessArray.fill(false);
+// 	for(var i = 0; i < guessArray.length; i++) {
+// 		if(wordArray[i] === "-") {
+// 			guessArray[i] = true;
+// 		}
+// 	}
 
-	document.getElementById("guessRow").innerHTML = makeDashes(word);
+// 	document.getElementById("guessRow").innerHTML = makeDashes(word);
 
-}
+// }
 
-function makeDashes(word, filler) {
-	var result = [];
+// function makeDashes(word, filler) {
+// 	var result = [];
 
-	for(var i = 0; i < word.length; i++) {
-		if(word[i] === "-")
-			result.push("-");
-		else result.push("_");
-	}
+// 	for(var i = 0; i < word.length; i++) {
+// 		if(word[i] === "-")
+// 			result.push("-");
+// 		else result.push("_");
+// 	}
 
-	return result.join(" ");
-}
+// 	return result.join(" ");
+// }
 
-function updateGuessWord() {
-	var result = [];
+// function updateGuessWord() {
+// 	var result = [];
 
-	for(var i = 0; i < word.length; i++) {
-		if(guessArray[i] === true)
-			result.push(wordArray[i]);
-		else result.push("_");
-	}
-	console.log(result);
+// 	for(var i = 0; i < word.length; i++) {
+// 		if(guessArray[i] === true)
+// 			result.push(wordArray[i]);
+// 		else result.push("_");
+// 	}
+// 	console.log(result);
 
-	return result.join(" ");
+// 	return result.join(" ");
 
-}
-
-function main() {
-	checkUserGuess();
-}
+// }
 
 function updateButtonUI(button, status) {
 	var update;
@@ -95,17 +97,8 @@ function updateButtonUI(button, status) {
 
 }
 
-function updateGuessUI(char) {
-	for(var i = 0; i < guessArray.length; i++) {
-		if(wordArray[i] === char)
-			guessArray[i] = true;
-	}
-
-}
-
-function updateHangmanUI() {
-	if(state < 6) {
-		state++;
+function updateHangmanUI(state, status) {
+	if(!status) {
 		document.getElementById("hangman").src = `./assets/images/hangman${state}.jpg`;
 	}
 }
@@ -145,4 +138,100 @@ function resetGame() {
 	/* Reset the phrase */
 
 	prepWord();
+}
+
+/* Attempt at OOP */
+function Hangman(dict) {
+	/* Initialize necessary variables */
+	this.state = 0,
+	this.dict = dict;
+	this.wordArray =[];
+	this.guessArray =[];
+
+}
+
+Hangman.prototype.prepareGame = function() {
+	console.log("preppin");
+	var word = dict[0].toUpperCase();
+	/* Processing the word */
+	this.wordArray = word.split("");
+	this.guessArray = new Array(this.wordArray.length);
+	for(var i = 0; i < this.wordArray.length; i++) {
+		if(this.wordArray[i] === " ")
+			this.guessArray[i] = this.wordArray[i];
+		else
+			this.guessArray[i] = "_";
+	}
+	//console.log(this.guessArray);
+}
+
+Hangman.prototype.getCurrentGuess = function() {
+	return this.guessArray.join("");
+}
+
+Hangman.prototype.guess = function(char) {
+	if(this.wordArray.includes(char))	{
+		/* Loop through the guessed word and replace the char */
+		for(var i = 0; i < this.guessArray.length; i++) {
+			if(this.wordArray[i] === char)
+				this.guessArray[i] = char;
+		}
+		return true;
+	}
+	else {
+		this.state >= 6 ? 6 : this.state++;
+		console.log(this.state);
+		return false;
+	}
+}
+
+Hangman.prototype.gameStatus = function() {
+	/* Loop to check win condition */
+
+	var equal = true;
+	for(var i = 0; i < this.wordArray.length; i++) {
+		if(this.wordArray[i] !== this.guessArray[i])
+			equal = false;
+	}
+
+	if(equal) {
+		console.log("Game won");
+		/* Do something in here */
+		return 1;	
+	}
+
+	/* Check if your game is lost */
+	else if (this.state >= 6) {
+		console.log("Game lost")
+		return -1;
+	}
+
+	/* On going game */
+	else return 0;
+
+}
+
+Hangman.prototype.resetGame = function() {
+	this.state = 0;
+	prepareGame();
+}
+
+/* Assigning function calls */
+
+var hangman = new Hangman(dict);
+
+document.getElementById("play-button").addEventListener("click", function() {
+	hangman.prepareGame();
+});
+
+document.getElementById("reset-button").addEventListener("click", resetGame);
+
+
+for(var i = 0; i < buttons.length; i++) {
+	buttons[i].addEventListener("click", function(event) {
+		var guess = hangman.guess(this.innerHTML);
+		updateButtonUI(this.innerHTML, guess);
+		updateHangmanUI(hangman.state, guess);
+		document.getElementById("guessRow").innerHTML = hangman.getCurrentGuess();
+	});
 }
